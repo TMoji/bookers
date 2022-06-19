0b071d4c83d4f32bad3fdee4046271408b2e0cea
EC2 Default User <105029527+TMoji@users.noreply.github.com>
2022-06-19 06:35:39 +0000

    ファイル作り直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get '/top'=>'books#top'
-  root to:'homes#top'
-  resources:lists
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  get '/top'=>'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
